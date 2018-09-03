62f7432f22a7fe95d60d98ed476a7498488c8272
AsemSaafan <asemsafan@yahoo.com>
2018-09-03 16:48:53 +0200

    Make Stateful Widgets to make variables

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,15 +1,28 @@
 // import flutter helper library;
 import 'package:flutter/material.dart';
 
+
+class App extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return AppState();
+  }
+}
 // Create Class
 // Using StatelessWidget because it will contain
 // the data which will not change
-class App extends StatelessWidget{
+class AppState extends State<App>{
+  int counter = 0;
+
   //build method which will run when this class is called
   Widget build(context){
     return new MaterialApp(
       // home attribute => first screen to show on the screen
       home: new Scaffold(
+        body: new Center(
+          child: new Text('$counter'),
+        ),
         appBar: new AppBar(
           title: new Text("Let's See Some Images"),
         ),
@@ -18,7 +31,9 @@ class App extends StatelessWidget{
           child: new Icon(Icons.add_a_photo),
           onPressed: (){
             //Print method appear in the console
-            print('Hello');
+            setState(() {
+              counter += 1 ;
+            });
           },
         ),
       ),
