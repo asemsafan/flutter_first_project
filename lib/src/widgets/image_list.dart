c13b5eb588a03f68e0d9e8cfc8d3194ff56328ba
AsemSaafan <asemsafan@yahoo.com>
2018-09-04 02:15:52 +0200

    - More about Layout: Column , Container , Padding Widget. - Make border complete. - Adding Image and the Title Related to it.

--- lib/src/widgets/image_list.dart
+++ lib/src/widgets/image_list.dart
@@ -2,20 +2,34 @@ import 'package:flutter/material.dart';
 import '../models/image_model.dart';
 
 class ImageList extends StatelessWidget {
-
   final List<ImageModel> images;
 
   ImageList(this.images);
 
-  Widget build(context){
-      return new ListView.builder(
-          itemCount:images.length ,
-          itemBuilder: (context , int index){
-            return new Container(
-              margin: EdgeInsets.all(20.0),
-              child: Image.network(images[index].url),
-            );
-          },
-      );
+  Widget build(context) {
+    return new ListView.builder(
+      itemCount: images.length,
+      itemBuilder: (context, int index) {
+        return buildImage(images[index]);
+      },
+    );
+  }
+
+  Widget buildImage(ImageModel image) {
+    return Container(
+      decoration: new BoxDecoration(
+          border: Border.all(
+        color: Colors.black26,
+      )),
+      margin: EdgeInsets.all(20.0),
+      padding: EdgeInsets.all(20.0),
+      child: new Column(
+        children: <Widget>[
+          Image.network(image.url),
+          new Padding(padding: EdgeInsets.only(bottom: 10.0)),
+          Text(image.title),
+        ],
+      ),
+    );
   }
 }
